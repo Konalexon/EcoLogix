d18b801b6b509e13b4b7d50d2c88e4b8af0b5bb9
Konalexon <konalexon@users.noreply.github.com>
2025-03-09 07:53:05 +0100

    feat: refine AppServiceProvider.php

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
+// update 71 
+
+// update 132 
+
+// update 387 
+
+// u370
