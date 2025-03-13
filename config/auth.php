b57b2435d3b3c73a2aea59fc30f5a18368d537c4
Konalexon <konalexon@users.noreply.github.com>
2025-03-13 14:11:12 +0100

    feat: driver setup

---
+++ config/auth.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    'defaults' => [
+        'guard' => 'web',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ],
+    ],
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+    ],
+
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+    ],
+
+    'password_timeout' => 10800,
+
+];
+
+// update 107 
+
+// update 205 
+
+// update 206 
+
+// update 227 
+
+// update 282 
+
+// update 346 
+
+// u292
+
+// u303
