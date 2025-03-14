71aba3e8e68d5c3dd8dae2499b8fab05e5154b99
Konalexon <konalexon@users.noreply.github.com>
2025-03-14 14:36:34 +0100

    feat: configure system params

---
+++ config/cors.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
+
+// update 209 
+
+// update 209 
+
+// update 357 
+
+// u117
+
+// u190
+
+// u191
+
+// u340
+
+// u374
