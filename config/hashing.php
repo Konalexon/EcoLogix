c5014ae0ed295b3c2310ae22e6ac72e5206f00f0
Konalexon <konalexon@users.noreply.github.com>
2025-03-15 15:53:49 +0100

    feat: driver setup

---
+++ config/hashing.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'driver' => 'bcrypt',
+    'bcrypt' => ['rounds' => env('BCRYPT_ROUNDS', 12)],
+];
+
+// update 56 
+
+// update 113 
+
+// update 158 
+
+// update 181 
+
+// update 187 
+
+// update 260 
