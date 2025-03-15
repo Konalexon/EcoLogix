a00dbaa5916dbe5cc6d5311bda889181acfb2b5c
Konalexon <konalexon@users.noreply.github.com>
2025-03-15 07:22:20 +0100

    feat: driver setup

---
+++ config/filesystems.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'default' => env('FILESYSTEM_DISK', 'local'),
+    'disks' => [
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+    ],
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+];
+
+// update 139 
+
+// update 151 
+
+// update 312 
+
+// update 370 
+
+// u132
