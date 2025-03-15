223d9cb8c04069e497f169b1ce16ffd6b7aa5c1b
Konalexon <konalexon@users.noreply.github.com>
2025-03-16 00:31:01 +0100

    feat: clarify config values

---
+++ config/logging.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+    'deprecations' => [
+        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+        'trace' => false,
+    ],
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+            'ignore_exceptions' => false,
+        ],
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+        'daily' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+    ],
+];
+
+// update 241 
+
+// update 264 
+
+// update 371 
+
+// u351
+
+// u412
