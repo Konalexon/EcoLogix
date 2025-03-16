08ebfd8869263f77b8a61d0ebf5f4b4c3d6c3149
Konalexon <konalexon@users.noreply.github.com>
2025-03-16 09:13:55 +0100

    feat: driver setup

---
+++ config/mail.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'default' => env('MAIL_MAILER', 'log'),
+    'mailers' => [
+        'smtp' => [
+            'transport' => 'smtp',
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+        ],
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+    ],
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@ecologix.pl'),
+        'name' => env('MAIL_FROM_NAME', 'EcoLogix'),
+    ],
+];
+
+// update 91 
+
+// update 184 
+
+// update 232 
+
+// update 406 
+
+// update 417 
+
+// u172
+
+// u239
+
+// u281
