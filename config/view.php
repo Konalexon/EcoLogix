66bec5091f0b729f51768ed32e4c9a2a7445690c
Konalexon <konalexon@users.noreply.github.com>
2025-03-17 21:02:12 +0100

    feat: driver setup

---
+++ config/view.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'paths' => [resource_path('views')],
+    'compiled' => env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))),
+];
+
+// update 159 
+
+// update 199 
+
+// update 272 
+
+// update 327 
+
+// u244
+
+// u253
+
+// u271
