6c995a1ab3039d6e7581f21a0892977510a3d4fe
Konalexon <konalexon@users.noreply.github.com>
2025-03-30 12:36:37 +0100

    feat: refine index.php

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__ . '/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
+
+// update 237 
+
+// update 255 
+
+// update 289 
+
+// update 352 
+
+// update 365 
+
+// update 365 
+
+// u81
+
+// u192
+
+// u310
+
+// u356
+
+// u385
