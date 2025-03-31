21b235ff3d143b158b36b55cfa685c10579596ad
Konalexon <konalexon@users.noreply.github.com>
2025-04-01 00:18:46 +0100

    feat: refine app.js

---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
+import '../css/app.css';
+
+// Create Vue app
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
+
+console.log('EcoLogix app mounted successfully!');
+
+// update 124 
+
+// update 149 
+
+// update 173 
+
+// update 195 
+
+// update 251 
+
+// update 279 
+
+// update 375 
+
+// u181
