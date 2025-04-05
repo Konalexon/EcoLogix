5b6ff67462485812553783511482d73ce8de3975
Konalexon <konalexon@users.noreply.github.com>
2025-04-05 16:07:47 +0100

    feat: fix AuthLayout.vue

---
+++ resources/js/layouts/AuthLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center p-4">
+        <slot />
+    </div>
+</template>
+
+// update 89 
+
+// update 125 
+
+// update 388 
+
+// update 410 
+
+// u327
