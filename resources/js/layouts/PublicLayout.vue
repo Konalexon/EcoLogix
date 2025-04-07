b5dad51828d571297a29dc36eabed1bb05f1b381
Konalexon <konalexon@users.noreply.github.com>
2025-04-07 06:57:28 +0100

    feat: optimize PublicLayout.vue

---
+++ resources/js/layouts/PublicLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="min-h-screen">
+        <slot />
+    </div>
+</template>
+
+// update 63 
+
+// update 67 
+
+// update 84 
+
+// update 218 
+
+// update 299 
+
+// update 318 
+
+// update 322 
+
+// update 333 
