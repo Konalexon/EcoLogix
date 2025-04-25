33416c4900e26c8207e7c681bd93813500d8bf2f
Konalexon <konalexon@users.noreply.github.com>
2025-04-25 20:42:39 +0100

    feat: optimize console.php

---
+++ routes/console.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+// update 258 
+
+// update 292 
+
+// update 330 
+
+// update 377 
+
+// update 382 
+
+// u246
