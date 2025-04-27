1ccdd8678dded2ded0c0b3e18d44688d09ee1d6b
Konalexon <konalexon@users.noreply.github.com>
2025-04-27 20:04:50 +0100

    feat: fix vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+});
+
+// update 84 
+
+// update 119 
+
+// update 215 
+
+// update 340 
+
+// update 383 
+
+// update 416 
+
+// update 419 
+
+// u148
+
+// u183
+
+// u380
+
+// u401
